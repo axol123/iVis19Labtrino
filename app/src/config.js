edfe751fc8306cdfdf89ccd1b74e17da81ff0fa4
lucasahlgren <35539195+lucasahlgren@users.noreply.github.com>
2019-02-12 12:05:21 +0100

    firebase init

---
+++ app/src/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+export const config = {
+  apiKey: "AIzaSyDiLG2CbyuBjm1YHw253iQk0zjmFEAyoSo",
+  authDomain: "infovis-838ec.firebaseapp.com",
+  databaseURL: "https://infovis-838ec.firebaseio.com",
+  projectId: "infovis-838ec",
+  storageBucket: "infovis-838ec.appspot.com",
+  messagingSenderId: "453826263185"
+};
