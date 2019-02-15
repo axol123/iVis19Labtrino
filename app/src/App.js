4f57e71e2794cfe6917ed9e83bddbfb7b717e675
evehed <evehed@kth.se>
2019-02-15 15:39:32 +0100

    add D3, simple version

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import LineChart from './LineChart';
+import BubbleChart from './BubbleChart';
+
 import * as firebase from "firebase";
 
 class App extends Component {
@@ -29,15 +32,15 @@ class App extends Component {
 		.equalTo("f58b8069-a2f5-4e2e-b8c2-a2bfb7fd642b")
 		.on("value", this.readData)
 
-		//Get value from Building2 where apartmentid = 5585bb8d-6e5d-4f01-91c0-b73632b6e2b0
-		building2.orderByChild("5")
-		.equalTo("5585bb8d-6e5d-4f01-91c0-b73632b6e2b0")
-		.on("value", this.readData)
-
-		//Get value from Building1 where apartmentid =
-		building3.orderByChild("5")
-		.equalTo("1cfa2f8f-ec5d-46da-a62a-ad2ba6be2058")
-		.on("value", this.readData)
+		// //Get value from Building2 where apartmentid = 5585bb8d-6e5d-4f01-91c0-b73632b6e2b0
+		// building2.orderByChild("5")
+		// .equalTo("5585bb8d-6e5d-4f01-91c0-b73632b6e2b0")
+		// .on("value", this.readData)
+		//
+		// //Get value from Building1 where apartmentid =
+		// building3.orderByChild("5")
+		// .equalTo("1cfa2f8f-ec5d-46da-a62a-ad2ba6be2058")
+		// .on("value", this.readData)
 
 
 	};
@@ -49,27 +52,31 @@ class App extends Component {
 		//Logs all of the three arrays fetched from firebase
 		console.log(obj.val());
 
+		array.push(obj.val());
+		console.log("array"+array)
 		this.setState({ data: array });
+
+		//Logs everything inside of the fetched value at the dictionary key "10"
+		console.log("dataaa: "+this.state.data[0]["10"]);
 	};
 
 	render() {
-		console.log(this.state.data);
+		console.log("dataaa"+this.state.data);
+
+		// <img src={logo} className="App-logo" alt="logo" />
+		// <p>	Edit <code>src/App.js</code> and save to reload.</p>
+		// <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+		// 	Learn React
+		// </a>
 
 		return (
 			<div className="App">
 				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
+				<p>LineChart</p>
+					<LineChart data={this.state.data}/>
+				<p>BubbleChart</p>
+					<BubbleChart/>
+
 				</header>
 			</div>
 		);
